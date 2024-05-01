73f933bb03d5261775a841aa8fc4586318d6d5b5
ahmedsalem2002m <ahmedsalem2002m@gmail.com>
2024-05-02 01:53:30 +0300

    add register adapter

--- lib/cubits/add_note_cubit/add_note_cubit.dart
+++ lib/cubits/add_note_cubit/add_note_cubit.dart
@@ -1,10 +1,17 @@
 import 'package:bloc/bloc.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:notes_app/models/note_model.dart';
 
 part  'add_note_state.dart';
 
 
 class AddNoteCubit extends Cubit<AddNoteState>{
   AddNoteCubit() : super(AddNoteInitial());
+
+
+
+  addNote(NoteModel note){
+
+  }
 
 }
